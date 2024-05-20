2e244c8e1519f5d0ce2878ff3f21599994fb5b03
Carl Malmz <callem217@gmail.com>
2024-05-21 01:47:10 +0200

    borked upload

--- app/routes/api.upload.tsx
+++ app/routes/api.upload.tsx
@@ -6,7 +6,6 @@ import {
   type ActionFunctionArgs,
   NodeOnDiskFile,
 } from '@remix-run/node';
-import { z } from 'zod';
 import sharp from 'sharp';
 import type { InferInsertModel } from 'drizzle-orm';
 import { image } from '~/lib/schema.server';
@@ -15,16 +14,9 @@ import { db } from '~/lib/db.server';
 import { ensureRole } from '~/lib/auth.server';
 import { uploadsPath } from '~/lib/storage/paths';
 import { commitUpload } from '~/lib/storage/image';
-import { getParams } from 'remix-params-helper';
+import { assertResponse } from '~/lib/utils';
 
 const imageTypes = ['image/jpeg', 'image/png'];
-
-const schema = z.object({
-  id: z.coerce.number(),
-  files: z
-    .array(z.instanceof(NodeOnDiskFile))
-    .min(1, 'At least 1 file is required'),
-});
 
 export async function action({ request }: ActionFunctionArgs) {
   const { claims } = await ensureRole(['write:image'])(request);
@@ -38,21 +30,24 @@ export async function action({ request }: ActionFunctionArgs) {
   );
 
   const formData = await parseMultipartFormData(request, uploadHandler);
-  const result = getParams(formData, schema);
-
-  if (!result.success) {
-    return result;
-  }
+  const id = Number(formData.get('id'));
+  assertResponse(!isNaN(id), 'Invalid album id');
+  const files = formData.getAll('files') as NodeOnDiskFile[];
+  assertResponse(files.length > 0, 'At least 1 file is required');
+  assertResponse(
+    files.every((file) => file instanceof NodeOnDiskFile),
+    'Invalid file'
+  );
 
   const insertdata = await Promise.all(
-    result.data.files.map(async (file, i) => {
+    files.map(async (file) => {
       const metadata = await sharp(file.getFilePath()).metadata();
       const exif_data = metadata.exif ? exif(metadata.exif) : (null as any);
       const taken_at =
         exif_data?.Image?.DateTime ?? new Date(file.lastModified) ?? new Date();
 
       return {
-        album_id: result.data.id,
+        album_id: id,
         mimetype: file.type,
         taken_by: claims?.sub,
         taken_by_name: claims?.name ?? claims?.email ?? null,
@@ -72,7 +67,7 @@ export async function action({ request }: ActionFunctionArgs) {
 
     await Promise.all(
       inserted.map(async (image, i) => {
-        commitUpload(result.data.files[i].getFilePath(), image);
+        commitUpload(files[i].getFilePath(), image);
       })
     );
   });
