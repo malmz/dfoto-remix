1a5e2e3784035d256cc15d01e56e214e87961556
Carl Malmz <callem217@gmail.com>
2024-05-24 21:42:24 +0200

    rework image page

--- app/routes/_main.album.$id.$imageId.tsx
+++ app/routes/_main.album.$id.$imageId.tsx
@@ -1,139 +1,48 @@
 import type { LoaderFunctionArgs } from '@remix-run/node';
-import {
-  Await,
-  Link,
-  useLoaderData,
-  useRouteLoaderData,
-} from '@remix-run/react';
+import { Await, Link, useLoaderData } from '@remix-run/react';
 import { format } from 'date-fns';
 import { sv } from 'date-fns/locale';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { Suspense } from 'react';
+import { ChevronLeft, CircleUser, Info } from 'lucide-react';
 import { Button } from '~/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
+
+import { getImage, getTags } from '~/lib/data.server';
+import { Avatar, AvatarFallback } from '~/components/ui/avatar';
+import { Badge } from '~/components/ui/badge';
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '~/components/ui/carousel';
-import { getImage } from '~/lib/data.server';
-import type { AlbumLoader } from './_main.album.$id';
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '~/components/ui/collapsible';
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const albumId = Number(params.id);
   const imageId = Number(params.imageId);
-  const image = await getImage(imageId);
-  if (!image) throw new Response('Not found', { status: 404 });
+  const tags = getTags(imageId);
+  const data = await getImage(imageId);
+  if (!data) throw new Response('Not found', { status: 404 });
   return {
-    image: image.image,
-    albumName: image.albumName,
+    image: data.image,
+    albumName: data.albumName,
     imageId,
     albumId,
+    tags,
   };
 }
 
-function ImageCarousel({
-  albumId,
-  imageId,
-}: {
-  albumId: number;
-  imageId: number;
-}) {
-  const { album } = useRouteLoaderData<AlbumLoader>('routes/_main.album.$id');
-
-  return (
-    <Suspense>
-      <Await resolve={album}>
-        {(album) => {
-          const currentIndex =
-            album?.images.findIndex((img) => img.id === imageId) ?? 0;
-          return (
-            <>
-              <div className='flex gap-4 sm:hidden'>
-                {currentIndex > 0 ? (
-                  <Button asChild variant='outline' size='icon'>
-                    <Link
-                      to={`/album/${albumId}/${
-                        album?.images[currentIndex - 1].id
-                      }`}
-                    >
-                      <ChevronLeft className='h-4 w-4'></ChevronLeft>
-                    </Link>
-                  </Button>
-                ) : null}
-
-                {currentIndex < (album?.images.length ?? 0) - 1 ? (
-                  <Button asChild variant='outline' size='icon'>
-                    <Link
-                      to={`/album/${albumId}/${
-                        album?.images[currentIndex + 1].id
-                      }`}
-                    >
-                      <ChevronRight className='h-4 w-4'></ChevronRight>
-                    </Link>
-                  </Button>
-                ) : null}
-              </div>
-              <div className='hidden px-20 sm:block sm:max-w-screen-sm md:max-w-screen-md'>
-                <Carousel
-                  className='w-full'
-                  opts={{
-                    startIndex: currentIndex,
-                  }}
-                >
-                  <CarouselContent>
-                    {album?.images.map((img, i) => (
-                      <CarouselItem
-                        className='basis-1/3  md:basis-1/5'
-                        key={img.id}
-                      >
-                        <div className='flex justify-center'>
-                          <Link
-                            to={`/album/${albumId}/${img.id}`}
-                            className='overflow-hidden rounded-lg'
-                          >
-                            <img
-                              src={`/api/image/${img.id}?thumbnail`}
-                              width={96}
-                              height={96}
-                              alt='fotografi'
-                              className='aspect-square h-24 w-24 object-cover transition-transform hover:scale-105'
-                              decoding='async'
-                              loading='lazy'
-                            />
-                          </Link>
-                        </div>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious />
-                  <CarouselNext />
-                </Carousel>
-              </div>
-            </>
-          );
-        }}
-      </Await>
-    </Suspense>
-  );
-}
-
 export default function Page() {
-  const { image, albumId, albumName } = useLoaderData<typeof loader>();
+  const { image, albumId, albumName, tags } = useLoaderData<typeof loader>();
 
   return (
     <>
-      <div className='mx-auto my-2 flex w-full max-w-screen-lg justify-between px-8'>
+      <div className='mx-auto my-2 flex w-full max-w-screen-lg justify-between px-2'>
         <Button asChild variant='ghost' size='sm'>
           <Link to={`/album/${albumId}`}>
             <ChevronLeft className='mr-2 h-4 w-4'></ChevronLeft>
-            <span>{albumName}</span>
+            {albumName}
           </Link>
         </Button>
       </div>
-      <div className='mx-auto my-2 max-w-screen-lg px-8'>
+      <div className='mx-auto my-2 max-w-screen-lg px-2'>
         <img
           src={`/api/image/${image.id}?preview`}
           width={1200}
@@ -142,15 +51,45 @@ export default function Page() {
           className='w-full object-contain rounded-md'
         />
       </div>
-      <div className='mx-auto my-2 flex w-full max-w-screen-lg justify-center'>
-        <ImageCarousel albumId={albumId} imageId={image.id}></ImageCarousel>
-      </div>
-      <div className='mx-auto my-2 w-full max-w-screen-lg px-8'>
-        <Card>
-          <CardHeader>
-            <CardTitle>Info</CardTitle>
-          </CardHeader>
-          <CardContent>
+
+      <Collapsible>
+        <div className='mx-auto my-2 w-full max-w-screen-sm px-2 flex flex-col gap-3'>
+          <div className='flex items-center justify-between flex-wrap'>
+            <div className='flex items-center gap-2'>
+              <Avatar>
+                <AvatarFallback>
+                  {image.taken_by_name?.slice(0, 2) ?? (
+                    <CircleUser className='h-5 w-5' />
+                  )}
+                </AvatarFallback>
+              </Avatar>
+              <span>{image.taken_by_name}</span>
+            </div>
+            <div className='flex items-center gap-2'>
+              <span className='text-muted-foreground'>
+                {format(image.taken_at, 'PPP, pp', { locale: sv })}
+              </span>
+              <CollapsibleTrigger asChild>
+                <Button variant='ghost' size='icon' className='rounded-full'>
+                  <Info className='h-4 w-4'></Info>
+                </Button>
+              </CollapsibleTrigger>
+            </div>
+          </div>
+
+          <div className='flex flex-wrap'>
+            <Await resolve={tags}>
+              {(ts) =>
+                ts.map((t) => (
+                  <Badge key={t.id} variant='secondary'>
+                    #{t.text}
+                  </Badge>
+                ))
+              }
+            </Await>
+          </div>
+
+          <CollapsibleContent>
             <div className='grid grid-cols-1 gap-2 text-sm sm:grid-cols-2'>
               <span>Fotograf</span>
               <span>{image.taken_by_name}</span>
@@ -158,16 +97,30 @@ export default function Page() {
               <span>{format(image.taken_at, 'PPP, pp', { locale: sv })}</span>
               <span>Format</span>
               <span>{image.mimetype}</span>
-              <span>Märke</span>
-              <span>{image.exif_data?.Image?.Make}</span>
-              <span>Model</span>
-              <span>{image.exif_data?.Image?.Model}</span>
-              <span>Lins</span>
-              <span>{image.exif_data?.Photo?.LensModel}</span>
+              {image.exif_data?.Image?.Make && (
+                <>
+                  <span>Märke</span>
+                  <span>{image.exif_data?.Image?.Make}</span>
+                </>
+              )}
+
+              {image.exif_data?.Image?.Model && (
+                <>
+                  <span>Model</span>
+                  <span>{image.exif_data?.Image?.Model}</span>
+                </>
+              )}
+
+              {image.exif_data?.Photo?.LensModel && (
+                <>
+                  <span>Lins</span>
+                  <span>{image.exif_data?.Photo?.LensModel}</span>
+                </>
+              )}
             </div>
-          </CardContent>
-        </Card>
-      </div>
+          </CollapsibleContent>
+        </div>
+      </Collapsible>
     </>
   );
 }
