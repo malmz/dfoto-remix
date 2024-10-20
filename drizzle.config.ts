cfe09ebe042a02be2cfbe354effdca0fca6b4d5f
Carl <callem217@gmail.com>
2024-10-21 01:42:15 +0200

    this never worked

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
 	dialect: 'postgresql',
-	schema: './app/lib/server/schema.ts',
+	schema: './app/lib/.server/schema.ts',
 	out: './migrations',
 	dbCredentials: {
 		url: process.env.DATABASE_URL ?? '',
--- vite.config.ts
+++ vite.config.ts
@@ -19,11 +19,6 @@ export default defineConfig({
 	},
 	plugins: [
 		expressDevServer(),
-		denyImports({
-			client: {
-				files: ['**/lib/server/*', '**/.server/*', '**/*.server.*'],
-			},
-		}),
 		envOnlyMacros(),
 		//remixDevTools(),
 		remix({
